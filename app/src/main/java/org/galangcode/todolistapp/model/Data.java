71dbf588333f1816b0f124938e5850c144269147
manggalang <galangpastibisa@gmail.com>
2024-04-03 11:08:29 +0800

    create model

--- app/src/main/java/org/galangcode/todolistapp/model/Data.java
+++ app/src/main/java/org/galangcode/todolistapp/model/Data.java
@@ -1,4 +1,40 @@
 package org.galangcode.todolistapp.model;
 
 public class Data {
+    private int id;
+    private String title, description;
+
+    public Data() {
+
+    }
+
+    public Data(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
